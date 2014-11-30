314d9776f20e66405beb2b2fdc0c1ab35465be6a
Greg <gparemsky@gmail.com>
2014-11-30 09:56:41 -0500

    Fogot git add -A, added titaniumNugget.png

---
+++ src/main/java/com/greg/modularexosuits/item/ItemTitaniumNugget.java
@@ -0,0 +1,15 @@
+package com.greg.modularexosuits.item;
+
+/**
+ * Created by Greg on 11/30/2014.
+ */
+public class ItemTitaniumNugget extends ItemMES {
+    public ItemTitaniumNugget(){
+        super();
+    }
+
+    @Override
+    public String getName() {
+        return "titaniumNugget";
+    }
+}
